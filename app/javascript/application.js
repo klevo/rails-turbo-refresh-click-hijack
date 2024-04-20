53dc624298bf6bf6bf1a08433d2aabca706af3d5
Robert Starsi <klevo@klevo.sk>
2024-04-20 17:10:44 +0200

    Log turbo visits to console, basic Item broadcasting

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+addEventListener("turbo:before-visit", (event) => {
+  console.debug("turbo:before-visit", event.detail)
+})
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  after_update_commit do
+    broadcast_refresh_to :items
+  end
 end
